d6a038e116bb83aad55412ff1dcb4c00a2d24578
seongeun-sheep <76435944+Yangseong-eun@users.noreply.github.com>
2022-11-07 13:30:50 +0900

    9주차 카드딜러

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WCardDealer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Card1 = new System.Windows.Forms.PictureBox();
+            this.BtnDeal = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Card1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Card1
+            // 
+            this.Card1.Location = new System.Drawing.Point(12, 12);
+            this.Card1.Name = "Card1";
+            this.Card1.Size = new System.Drawing.Size(260, 326);
+            this.Card1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Card1.TabIndex = 0;
+            this.Card1.TabStop = false;
+            // 
+            // BtnDeal
+            // 
+            this.BtnDeal.Location = new System.Drawing.Point(21, 364);
+            this.BtnDeal.Name = "BtnDeal";
+            this.BtnDeal.Size = new System.Drawing.Size(239, 65);
+            this.BtnDeal.TabIndex = 1;
+            this.BtnDeal.Text = "카드분배";
+            this.BtnDeal.UseVisualStyleBackColor = true;
+            this.BtnDeal.Click += new System.EventHandler(this.OnDeal);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 441);
+            this.Controls.Add(this.BtnDeal);
+            this.Controls.Add(this.Card1);
+            this.Name = "Form1";
+            this.Text = "카드 딜러";
+            ((System.ComponentModel.ISupportInitialize)(this.Card1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox Card1;
+        private Button BtnDeal;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+namespace WCardDealer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDeal(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            int n = r.Next(52);
+
+            Image? img = Properties.Resources.ResourceManager.GetObject(GetCardName(n)) as Image;
+
+            Card1.Image = img;
+        }
+        private string GetCardName(int c)
+        {
+            string shape = "";
+            switch (c / 13) //c를 13으로 나눈 나머지로 처리
+            {
+                case 0: shape = "spades"; break;
+                case 1: shape = "diamonds"; break;
+                case 2: shape = "hearts"; break;
+                case 3: shape = "clubs"; break;
+                default: shape = "error"; break; //default로 확실히 에러를 잡는다
+            }
+
+            string number = "";
+            switch (c % 13)
+            {
+                case 0: number = "ace"; break;
+                case int n when (n > 0 && n < 10):
+                    number = (c % 13 + 1).ToString(); break;
+                case 10: number = "jack"; break;
+                case 11: number = "queen"; break;
+                case 12: number = "king"; break;
+                default: number = "error"; break;
+            }
+
+            //return number + "_of_" + shape;
+            //return string.Format("{0}_of_{1}", number, shape);
+            return $"{number}_of_{shape}";
+        }
+    }
+}
